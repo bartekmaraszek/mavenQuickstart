3995fcf117d27426e133c21e64d8aa52f709e4dd
Bartek Maraszek <b.maraszek@gmail.com>
2017-12-30 23:50:04 +0000

    Add System.out.println() tests

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,11 +1,26 @@
 package ${groupId};
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
 
-    public void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello world!");
+        App app = new App();
+        app.readFile("resources.csv");
+    }
 
+    public void readFile(String fileName){
+        File file = new File(Thread.currentThread().getContextClassLoader().getResource(fileName).getFile());
+        try(Scanner s = new Scanner(file)) {
+            while(s.hasNextLine()) {
+                System.out.println(s.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
